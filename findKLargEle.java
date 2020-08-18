92a70b86472a765e3463c35fd90304fa607718b1
Abhay <chauhan27.abhay@gmail.com>
2020-08-18 15:16:39 +0530

    find K Largest Elements

---
+++ findKLargEle.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+class findKLargEle{
+
+    public static void main(String[] args){
+        int[] arr = {2, 10, 5, 17, 7, 18, 6, 4};
+        int k=3;
+       PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+       for(int i=0; i<arr.length; i++){
+           if(i<k){
+               pq.add(arr[i]);
+           }else{
+               if(pq.peek()< arr[i]){
+                   pq.remove();
+                   pq.add(arr[i]);
+               }
+           }
+       }
+
+       while(pq.size() > 0){
+           System.out.println(pq.remove());
+       }
+    }
+}
